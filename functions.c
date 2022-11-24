f40108f58106d7c288c32f999da2557f7d703ebd
mershark <mersharktakyi@gmail.com>
2022-11-24 11:12:04 -0800

    update

--- functions.c
+++ functions.c
@@ -4,7 +4,7 @@
 
 /**
  * print_char - Prints a char
- * @types: Lists a of arguments
+ * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: Width
